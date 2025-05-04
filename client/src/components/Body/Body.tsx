5948c011527a1ac48fe40f1e674ca6d33cecf656
grbvtsk <ngraboveckij@gmail.com>
2025-05-04 17:11:01 +0300

    added scrolling to last message

--- client/src/components/Body/Body.tsx
+++ client/src/components/Body/Body.tsx
@@ -1,35 +1,53 @@
 import SendMessageBlock from "./SendMessageBlock";
 import ReceiveMessageBlock from "./ReceiveMessageBlock";
-import {BodyProps} from "../../types/BodyComponents/BodyProps.interface.ts";
-import React from "react";
+import { BodyProps } from "../../types/BodyComponents/BodyProps.interface.ts";
+import React, { useEffect, useRef } from "react";
 
+const Body: React.FC<BodyProps> = ({
+  messages,
+  recipientUser,
+  loadMessages,
+}) => {
+  const messagesEndRef = useRef<HTMLDivElement | null>(null);
 
-const Body: React.FC<BodyProps> = ({messages, recipientUser,loadMessages}) => {
-    return (
-        <div className="flex flex-col h-screen">
-            <div className="flex-1 p-4 bg-gray-50">
-                {recipientUser && (
-                    <div className="space-y-4">
-                        {messages.map((element) => {
-                            if (element.sender_name === localStorage.getItem('user') &&
-                                element.recipient_name===recipientUser) {
-                                return (
-                                    <SendMessageBlock key={element.id} element={element} loadMessages={loadMessages}/>
-                                );
-                            } else if (element.sender_name === recipientUser &&
-                                element.recipient_name === localStorage.getItem('user')) {
-                                return (
-                                    <ReceiveMessageBlock key={element.id} element={element}/>
-                                );
-                            } else {
-                                return null;
-                            }
-                        })}
-                    </div>
-                )}
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="flex-1 p-4 bg-gray-50">
+        {recipientUser && (
+          <div className="space-y-4">
+            {messages.map((element) => {
+              if (
+                element.sender_name === localStorage.getItem("user") &&
+                element.recipient_name === recipientUser
+              ) {
+                return (
+                  <SendMessageBlock
+                    key={element.id}
+                    element={element}
+                    loadMessages={loadMessages}
+                  />
+                );
+              } else if (
+                element.sender_name === recipientUser &&
+                element.recipient_name === localStorage.getItem("user")
+              ) {
+                return (
+                  <ReceiveMessageBlock key={element.id} element={element} />
+                );
+              } else {
+                return null;
+              }
+            })}
+            <div ref={messagesEndRef} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Body;
