b32333a11c8581d08bdb943652c0b5a8af2c427f
grbvtsk <ngraboveckij@gmail.com>
2025-06-23 15:19:13 +0300

    All check

--- server/index.js
+++ server/index.js
@@ -17,5 +17,5 @@ app.use("/api/summarize", summarizeRouter);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.listen(PORT, () => {
-  console.log("App started");
+  console.log("app started");
 });
